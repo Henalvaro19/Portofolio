1f780aa840a6550ee5f3889c18bfa43b1a9df457
Henalvaro19 <henalvarofarrel@gmail.com>
2026-02-01 19:59:04 +0700

    Tambah sertifikat

--- script.js
+++ script.js
@@ -167,3 +167,25 @@ faders.forEach((fader) => {
 
 // // update saat resize window
 // window.addEventListener('resize', updateSlider);
+
+const slides = document.querySelectorAll(".slide");
+let current = 0;
+
+function showSlide(index) {
+    slides.forEach(slide => slide.classList.remove("active"));
+    slides[index].classList.add("active");
+}
+
+function nextSlide() {
+    if (current < slides.length - 1) {
+        current++;
+        showSlide(current);
+    }
+}
+
+function prevSlide() {
+    if (current > 0) {
+        current--;
+        showSlide(current);
+    }
+}
